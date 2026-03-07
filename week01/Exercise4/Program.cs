c10f3c4e989ff24a9da9a9d26653478a1d6fee3e
Scarlett Pérez <tuemail@byupathway.edu>
2026-03-07 16:32:35 -0600

    Finished CSE210 Exercises 1-5

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+         List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        // Core calculations
+        int sum = 0;
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        // Output results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
